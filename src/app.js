658915f5e7b34562b6bbf72c1289ce1108bb3db3
Aaron Graham <aarongraham518@gmail.com>
2020-10-28 23:29:11 -0400

    edit test app.js

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ const NotFoundPage = () => (
 
 const Header = () => (
     <header>
-        <h1>Expensify</h1>
+        <h1>Expensify.</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard </NavLink>
         <NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink>
         <NavLink to="/edit" activeClassName="is-active"> Edit Expense </NavLink>
